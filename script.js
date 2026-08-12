4f08f18a22492511c5e654c2bfacc5252ea62c51
bjkp-org <12ajeetchaudhary@gmail.com>
2026-08-12 20:48:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('memberForm').addEventListener('submit',function(e){
- e.preventDefault();
- const f=new FormData(this);
- const id='BJKP-'+Date.now().toString().slice(-8);
- document.getElementById('result').innerHTML='<b>आवेदन प्राप्त हुआ!</b><br>आवेदन ID: '+id+'<br>आपका वास्तविक ऑनलाइन रिकॉर्ड सुरक्षित रखने के लिए वेबसाइट को backend/database से जोड़ना आवश्यक है।';
- this.reset();
-});
